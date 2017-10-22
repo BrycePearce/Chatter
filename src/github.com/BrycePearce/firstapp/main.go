fdadb2bbdb231c496590fac654763bd9a14e890e
BrycePearce <bpearce@angelo.edu>
2017-10-21 23:53:32 -0500

    new chatbox/webserver/goroutines boilerplate

--- src/github.com/BrycePearce/firstapp/main.go
+++ src/github.com/BrycePearce/firstapp/main.go
@@ -2,89 +2,49 @@ package main // main is the entry point of the application
 
 // go run x y
 import (
-	"net/http"
-	"time"
 	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{} // upgrades the http request to a websocket
 
-func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res, req, "../../../../index.html")
-	})
-
-	// echo
-	http.HandleFunc("/v1/ws", func(res http.ResponseWriter, req *http.Request) {
-		var conn, _ = upgrader.Upgrade(res, req, nil) // create the websocket connection with (nil allows various headers to be passed back and forth between websocket) http://www.gorillatoolkit.org/pkg/websocket#Upgrader.Upgrade
-		go func(conn *websocket.Conn) { // go routine
-			for { // this for continuously listens for messages
-			mType, msg, _ := conn.ReadMessage() // reads a message coming in from the client, takes the message/type of the message (types ex: ping/pong/text/binary)
-
-			conn.WriteMessage(mType, msg) // write the message back to the client
-			}
-		}(conn)
-	})
-
-	// read the request message on GO side
-	http.HandleFunc("/v2/ws", func(res http.ResponseWriter, req *http.Request) {
-		var conn, _ = upgrader.Upgrade(res, req, nil)
-		go func(conn *websocket.Conn) {
-			for {
-				_, msg, _ := conn.ReadMessage() // note: underscores are just placeholders for methods you aren't going to be using (mType, msg, err -> _, msg, _)
-				println(string(msg))
-			}
-		}(conn)
-	})
-
-	// every 5 seconds, sends a message to the client
-	http.HandleFunc("/v3/ws", func(res http.ResponseWriter, req *http.Request) {
-		var conn, _ = upgrader.Upgrade(res, req, nil)
-		go func(conn *websocket.Conn) {
-			ch := time.Tick(5 *time.Second)
-
-			for range ch { // send an instance 
-				conn.WriteJSON(myStruct {
-					Username: "Node231",
-					FirstName: "Node",
-					LastName: "231",
-				})
-			}
-		}(conn)
-	})
-
-	// v3 only writes to socket. This version will recieve a message FROM the socket, in this case, a closing connection which we read 
-	http.HandleFunc("/v4/ws", func(res http.ResponseWriter, req *http.Request) {
-		var conn, _ = upgrader.Upgrade(res, req, nil)
-		go func(conn *websocket.Conn) {
-			for {
-				_, _, err := conn.ReadMessage() // read all incoming messages, if it recieves a close message is recieved from the client, it will populate error object
-				if err != nil { // so if error is populated
-					conn.Close() // we close the connection, otherwise the connection will stay open forever and consume server resources
-				}
-			}
-		}(conn)
-		
-		go func(conn *websocket.Conn) {
-			ch := time.Tick(2 *time.Second)
-
-			for range ch {
-				conn.WriteJSON(myStruct {
-					Username: "Node231",
-					FirstName: "Node",
-					LastName: "231",
-				})
-			}
-		}(conn)
-	})
-	
-
-	// serve it up
-	http.ListenAndServe(":3000", nil)
+// Hub is GO's hashmap, in this case it stores a map of all connected clients, and broadcasts messages to them all
+type Hub struct {
+	clients      map[*Client]bool
+	broadcast    chan []byte
+	addClient    chan *Client
+	removeClient chan *Client
 }
 
-type myStruct struct {
-	Username string `json:"username"`
-	FirstName string `json:"firstName"`
-	LastName string `json:"lastName"`
+// initalize a new hub
+var hub = Hub{
+	broadcast:    make(chan []byte),
+	addClient:    make(chan *Client),
+	removeClient: make(chan *Client),
+	clients:      make(map[*Client]bool),
+}
+
+// todo: figure out why two case conn
+// Runs forever as a goroutine
+func (hub *Hub) start() {
+	for { // this for loop continously listens for messages
+		// select is similar to switch/case
+		select {
+		case conn := <-hub.addClient: // case for adding a client. <- adds 'hub.addClient'as an item to conn
+			hub.clients[conn] = true
+		case conn := <-hub.removeClient: // case for removing a client. <- adds 'hub.removeClient' as an item to conn
+			if _, ok := hub.clients[conn]; ok {
+				delete(hub.clients, conn) // The delete built-in function deletes the element with the specified key (m[key]) from the map. func delete(m map[Type]Type1, key Type)
+				close(conn.send)          // close indicated that no more values will be sent on it https://gobyexample.com/closing-channels
+			}
+		case message := <-hub.broadcast: // case for sending a message to all clients
+			for conn := range hub.clients { // for declares the loop, range iterates through the clients
+				select {
+				case conn.send <- message:
+				default:
+					close(conn.send)
+					delete(hub.clients, conn)
+				}
+			}
+		}
+	}
 }

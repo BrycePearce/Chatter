3d4897c41e8ba4c0b2acd8a83296bfdd04fb6135
BrycePearce <bpearce@angelo.edu>
2017-10-22 20:43:45 -0500

    added structure for client read/write, etc

--- src/github.com/BrycePearce/firstapp/main.go
+++ src/github.com/BrycePearce/firstapp/main.go
@@ -3,6 +3,7 @@ package main // main is the entry point of the application
 // go run x y
 import (
 	"github.com/gorilla/websocket"
+	"net/http"
 )
 
 var upgrader = websocket.Upgrader{} // upgrades the http request to a websocket
@@ -10,13 +11,13 @@ var upgrader = websocket.Upgrader{} // upgrades the http request to a websocket
 // Hub is GO's hashmap, in this case it stores a map of all connected clients, and broadcasts messages to them all
 type Hub struct {
 	clients      map[*Client]bool
-	broadcast    chan []byte
+	broadcast    chan []byte // chan-> channels which are conduits through which you can send and recieve values with <- syntax https://tour.golang.org/concurrency/2
 	addClient    chan *Client
-	removeClient chan *Client
+	removeClient chan *Clienta
 }
 
 // initalize a new hub
-var hub = Hub{
+var hub = Hub {
 	broadcast:    make(chan []byte),
 	addClient:    make(chan *Client),
 	removeClient: make(chan *Client),
@@ -28,7 +29,7 @@ var hub = Hub{
 func (hub *Hub) start() {
 	for { // this for loop continously listens for messages
 		// select is similar to switch/case
-		select {
+		select { // formally, select waits/blocks a message until one of its cases can run
 		case conn := <-hub.addClient: // case for adding a client. <- adds 'hub.addClient'as an item to conn
 			hub.clients[conn] = true
 		case conn := <-hub.removeClient: // case for removing a client. <- adds 'hub.removeClient' as an item to conn
@@ -47,4 +48,67 @@ func (hub *Hub) start() {
 			}
 		}
 	}
+}
+
+// we pass data through this channel from the client functions below, which broadcasts the sent messages
+type Client struct {
+	ws *websocket.Conn // define this here so we can reference it in places such as write()
+	// Hub passes broadcast messages to this channel
+	send chan []byte // todo: figure out what this is doing. Think it just creates a byte array that can reallocate memory over and over, thus speeding things up a bit - http://openmymind.net/Introduction-To-Go-Buffered-Channels/
+}
+
+// Hub broadcasts a new message and this fires 
+func (c *Client) write() {
+	// make sure to close the connection incase the loop exits
+	defer func() { // defers this function from running until the parent function (write()) is done running. So this will only run if the for already exists, which would cause a problem
+			c.ws.Close()
+	}()
+
+	for { // listening for messages
+			select {
+			case message, ok := <-c.send: // pass in message, ok from c.send (from Client passed into write()), and create a case using both variables
+					if !ok {
+							c.ws.WriteMessage(websocket.CloseMessage, []byte{}) // http://www.gorillatoolkit.org/pkg/websocket#Conn.WriteMessage
+							return
+					}
+					c.ws.WriteMessage(websocket.TextMessage, message)
+			}
+	}
+}
+
+// New message received so pass it to the Hub 
+func (c *Client) read() {
+	defer func() {
+			hub.removeClient <- c // define client as removeClient as defined in our Hub
+			c.ws.Close() // close the websocket connection
+	}()
+
+	for {
+			_, message, err := c.ws.ReadMessage() // **************
+			if err != nil {
+					hub.removeClient <- c
+					c.ws.Close()
+					break
+			}
+
+			hub.broadcast <- message
+	}
+}
+
+func wsPage(res http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(res, req, nil)
+	if err != nil {
+			http.NotFound(res, req)
+			return
+	}
+
+	client := &Client{
+			ws:   conn,
+			send: make(chan []byte),
+	}
+
+	hub.addClient <- client
+
+	go client.write()
+	go client.read()
 }
